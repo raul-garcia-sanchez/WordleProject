266882998d07f8800667593bbd2ddc44d5988b97
Brahian412 <bmonsalvemunoz.cf@iesesteveterradas.cat>
2022-10-14 18:30:15 +0200

    LandingPage sin css y falta tablas

---
+++ landingPage.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <title>Wordle</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+</head>
+<body>
+    <header>
+        <h1>WORDLE</h1>
+    </header>
+
+    <?php
+
+    ?>
+
+    <p>Endevina la <strong>WORDLE</strong> en 6 intents. <br>
+        Cada fila ha de ser una paraula vàlida de 5 lletres. Premi el botó intro per enviar. <br>
+        Després de cada suposició, el color de les fitxes canviarà <br>
+        per mostrar què tan a prop ets de la paraula. <br>
+        <br>
+        <strong>Exemples</strong> <br>
+        La lletra T està en la paraula i en el lloc correcte -> VERD <br>
+        La lletra R està en la paraula i en el lloc equivocar -> GROC <br>
+        La lletra S no està en la paraula i enlloc -> GRIS
+    </p>
+
+    <form method="POST">
+        <input type="text" name="" id="" placeholder="Escrigui el seu nom">
+        <button type="submit">Jugar</button>
+    </form>
+</body>
+</html>
