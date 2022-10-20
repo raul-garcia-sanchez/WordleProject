30d4a03909700c1204637bd0c15e0f11bea77a09
Brahian412 <bmonsalvemunoz.cf@iesesteveterradas.cat>
2022-10-20 17:40:36 +0200

    Cambio de pagina inicial a index.php

--- landingPage/index.php
+++ landingPage/index.php
@@ -4,7 +4,7 @@
     <title>Wordle</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./landingPage.css">
+    <link rel="stylesheet" href="./index.css">
 </head>
 <body>
     <header>
