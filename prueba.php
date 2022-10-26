3bb0b9dd9bb2fb69f26f3a54b87f1d4ec39d28bb
Andres Villca <andres24vt@gmail.com>
2022-10-26 14:35:15 +0200

    warframe
    se ha añadido el wireframe

--- prueba.php
+++
@@ -1,7 +0,0 @@
-<?php
-$firstRowKeyboard = array("Q","W","E","R","T","Y","U","I","O","P");
-$arrayNueva= explode(",",'Q,W,E,R,T,Y,U,I,O,P');
-print_r($arrayNueva);
-echo "<br>";
-print_r($firstRowKeyboard);
-?>
